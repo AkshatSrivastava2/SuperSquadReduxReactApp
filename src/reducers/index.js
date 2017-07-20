455946853b5ef0f3e986a6f7605378c9466e432a
AkshatSrivastava2 <akshatsrivastava2@gmail.com>
2017-07-20 22:20:28 +0530

    Helper function and modularity increased

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,9 @@
-import characters_json from '../data/characters.json';
-import { ADD_CHARACTER } from '../actions';
 import { combineReducers } from 'redux';
+import characters from './characters_reducer.js';
+import heroes from './heroes_reducer.js';
 
-function characters(state=characters_json,action){
-    switch(action.type){
-        case ADD_CHARACTER:
-            let characters=state.filter(item=>item.id !==action.id);
-            return characters;
-        default:
-            return state;
-    }
-}
-function heroes(state=[],action){
-    switch(action.type){
-        case ADD_CHARACTER:
-            let heroes=[...state,createCharacter(action.id)];
-            return heroes;
-        default:
-            return state;
-    }
-}
-function createCharacter(id)
-{
-    let characters=characters_json.find(c => c.id === id);
-}
-export default characters;
+const rootReducer =combineReducers({
+    characters,
+    heroes
+})
+export default rootReducer;
